54a9efaca4f88e56f76d993ba788d5f716cfe476
khanhtd8998 <trankhanhhy199x@gmail.com>
2024-06-02 20:29:52 +0700

    fix: fix validation category

--- src/validations/category.js
+++ src/validations/category.js
@@ -5,5 +5,5 @@ export const CategorytSchema = new joi.object({
     slug: joi.string().required().min(3).max(255).empty(),
     description: joi.string().required().min(3).empty(),
     hide: joi.boolean(),
-
+    image: joi.string().required().min(3).empty(),
 })  
